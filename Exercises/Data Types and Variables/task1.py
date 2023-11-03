f3e3d4b7c10c0addf35735093c4dcb4da4de35c6
29008922 <haron.murumba22@gmail.com>
2023-11-03 09:05:15 -0700

    python data structures

--- Exercises/Data Types and Variables/task1.py
+++ Exercises/Data Types and Variables/task1.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
 # Write a Python program that asks the user to enter their name and age, and then prints a message
 # saying "Hello, [name]! You are [age] years old."
 # Write your program below
+
+#ask user to enter name and age
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your second name: ')
+name = first_name+" "+ last_name
+age = input("Enter your age: ")
+print("Hello" " "+name +" "+"you are"+" "+ (age)+" " +"years old")
+
