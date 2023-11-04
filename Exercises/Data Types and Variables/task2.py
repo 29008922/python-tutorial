6a1ecfca62a0e854d710c0b0c805ec3b1f470f11
29008922 <haron.murumba22@gmail.com>
2023-11-03 22:30:52 -0700

    area of the rectangle

--- Exercises/Data Types and Variables/task2.py
+++ Exercises/Data Types and Variables/task2.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
 # Write a Python program that calculates the area of a rectangle. The program should ask the user to enter
 # the length and width of the rectangle, and then print the area.
 # Write your program below
+
+#enter the dimension of a rectangle and print area
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enterthe width of that rectangle: "))
+area = length * width
+print("the area of the rectangle is: "+" "+ str(area)+ " " + "cm2") 
