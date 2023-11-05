3aada627d0390fb669ba6c842469eda020b3d840
29008922 <haron.murumba22@gmail.com>
2023-11-04 23:29:59 -0700

    temperature conversion

--- Exercises/Data Types and Variables/task3.py
+++ Exercises/Data Types and Variables/task3.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
 # Write a Python program that converts Fahrenheit temperatures to Celsius temperatures.
 # The program should ask the user to enter a temperature in Fahrenheit,and then print the
 # equivalent temperature in Celsius.The conversion formula is: C = (F - 32) * 5/9
 # write your program below
+
+#conversion of farein height to tempereature in 0C
+
+Fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+Celsius = (Fahrenheit - 32) * 5/9
+print('%.2f F = %.2f 0C ' %(Fahrenheit, Celsius))
