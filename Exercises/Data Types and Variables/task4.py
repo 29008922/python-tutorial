262b50f41632baf85beffceda3183496cc9eff21
29008922 <haron.murumba22@gmail.com>
2023-11-06 05:30:00 -0800

    more practice

---
+++ Exercises/Data Types and Variables/task4.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+name = "haron wekesa murumba"
+age = input("Enter your age: ")
+designation = input("enter your permanent job: ")
+print(f"my name is {name} " )
+print(f"your age is {age} and iam a {designation} ")
